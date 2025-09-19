118a1d409f8c56a90a1b62f818047ec4134e3092
ClumsyNoodlester <101434461+ClumsyNoodlester@users.noreply.github.com>
2025-09-19 10:50:23 +0100

    Mudar contador de minutos para segundos

--- basicExample/main.c
+++ basicExample/main.c
@@ -3,12 +3,12 @@
 
 int main(void) {
     int i = 8;
-    printf ("Quantos minutos faltam?\n");
+    printf ("Quantos segundo(s) faltam?\n");
     scanf(" %d", &i);
     while ( i != 0)
     {
-        printf("Faltam %i minutos para terminar teste\n", i);
-        sleep(60);
+        printf("Faltam %i segundo(s) para terminar teste\n", i);
+        sleep(1);
         i--;
     }
     return 0;
